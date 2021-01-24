0827a0f8525b34601be3f77b4a4f6646d4f0ed63
Ivan Malchanau <ivan.molchanoff@gmail.com>
2021-01-24 16:22:06 +0300

    operation arguments

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,6 +3,8 @@ import { GraphQLServer } from 'graphql-yoga';
 // Type definitions (schema)
 const typeDefs = `
     type Query {
+      add(a: Float!, b:Float! ): Float!
+      greeting(name: String, position: String): String!
       me: User!
       lastPost: Post
     }
@@ -25,6 +27,16 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
+    add(parent, args, ctx, info) {
+      return args.a + args.b;
+    },
+    greeting(parent, args, ctx, info) {
+      if (args.name && args.position) {
+        return `Hello, ${args.name} You are my favorite ${args.position}`;
+      } else {
+        return 'Hello';
+      }
+    },
     me() {
       return {
         id: '12345',
