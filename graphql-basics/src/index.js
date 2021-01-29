164b40147dbaaa3885812cf42946f40523c05932
Ivan Malchanau <ivan.molchanoff@gmail.com>
2021-01-29 22:30:29 +0300

    mutation for deleting user with relation data

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,7 +2,7 @@ import { GraphQLServer } from 'graphql-yoga';
 import { v4 as uuidv4, v4 } from 'uuid';
 
 //Demo user Data
-const users = [
+let users = [
   {
     id: '1',
     name: 'Ivan',
@@ -23,7 +23,7 @@ const users = [
 ];
 
 //Demo posts Data
-const posts = [
+let posts = [
   {
     id: '1',
     title: 'Post 1q',
@@ -48,7 +48,7 @@ const posts = [
 ];
 
 //Demo comments data
-const comments = [
+let comments = [
   {
     id: '111',
     text: 'Qwerty1',
@@ -105,6 +105,7 @@ const typeDefs = `
 
     type Mutation {
         createUser(data: CreateUserInput) : User!
+        deleteUser(id:ID!) : User!
         createPost(data: CreatePostInput): Post!
         createComment(data: CreateCommentInput) : Comment!
     }
@@ -212,6 +213,27 @@ const resolvers = {
       users.push(user);
 
       return user;
+    },
+    deleteUser(parent, args, ctx, info) {
+      const userIndex = users.findIndex((user) => user.id === args.id);
+      if (userIndex === -1) {
+        throw new Error('User not found');
+      }
+
+      posts = posts.filter((post) => {
+        const match = post.author === args.id;
+
+        if (match) {
+          comments = comments.filter((comment) => comment.post !== post.id);
+        }
+        return !match;
+      });
+
+      comments = comments.filter((comment) => comment.author !== args.id);
+
+      const deletedUsers = users.splice(userIndex, 1);
+
+      return deletedUsers[0];
     },
     createPost(parent, args, ctx, info) {
       const userExists = users.some((user) => user.id === args.data.author);
