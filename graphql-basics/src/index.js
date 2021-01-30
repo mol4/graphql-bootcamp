d6ce79a7f1175788f04b55fe47942e182789a8ae
Ivan Malchanau <ivan.molchanoff@gmail.com>
2021-01-30 22:34:42 +0300

    mutation for deleting post with related data

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -107,6 +107,7 @@ const typeDefs = `
         createUser(data: CreateUserInput) : User!
         deleteUser(id:ID!) : User!
         createPost(data: CreatePostInput): Post!
+        deletePost(id:ID!) : Post!
         createComment(data: CreateCommentInput) : Comment!
     }
 
@@ -248,6 +249,16 @@ const resolvers = {
       posts.push(post);
 
       return post;
+    },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex((post) => post.id === args.id);
+      if (postIndex === -1) {
+        throw new Error('Post not found');
+      }
+
+      comments = comments.filter((comment) => comment.post !== args.id);
+      const deletedPosts = posts.splice(postIndex, 1);
+      return deletedPosts[0];
     },
     createComment(parent, args, ctx, info) {
       const userExists = users.some((user) => user.id === args.data.author);
