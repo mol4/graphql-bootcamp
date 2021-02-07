2747f6ed02240fb2fe0016191702bb6657f716c4
Ivan Malchanau <ivan.molchanoff@gmail.com>
2021-02-07 22:26:58 +0300

    mutation for update post and comment

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -80,6 +80,7 @@ const Mutation = {
 
     return post;
   },
+
   deletePost(parent, args, { db }, info) {
     const postIndex = db.posts.findIndex((post) => post.id === args.id);
     if (postIndex === -1) {
@@ -90,6 +91,26 @@ const Mutation = {
     const deletedPosts = db.posts.splice(postIndex, 1);
     return deletedPosts[0];
   },
+
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+    const post = db.posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new Error('Post not found');
+    }
+
+    if (typeof data.title === 'string') {
+      post.title = data.title;
+    }
+    if (typeof data.body === 'string') {
+      post.body = data.body;
+    }
+    if (typeof data.published === 'boolean') {
+      post.published = data.published;
+    }
+  },
+
   createComment(parent, args, { db }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
     const postExists = db.posts.some(
@@ -116,6 +137,19 @@ const Mutation = {
     }
     const deletedComments = db.comments.splice(commentIndex, 1);
     return deletedComments[0];
+  },
+
+  updateComment(parent, args, { db }, info) {
+    const { id, data } = args;
+    const comment = db.comments.find((comment) => comment.id === id);
+
+    if (!comment) {
+      throw new Error('comment not found');
+    }
+
+    if (typeof data.text !== 'string') {
+      comment.text = data.text;
+    }
   },
 };
 
