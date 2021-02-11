6d14289d946d785ea1469411aea348388aef82c3
Ivan Malchanau <ivan.molchanoff@gmail.com>
2021-02-11 23:14:20 +0300

    very first subscription

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,24 +1,29 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
 import Post from './resolvers/Post';
 import User from './resolvers/User';
 import Comment from './resolvers/Comment';
 
 import db from './db';
 
+const pubsub = new PubSub();
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     Post,
     User,
     Comment,
   },
   context: {
     db,
+    pubsub,
   },
 });
 
