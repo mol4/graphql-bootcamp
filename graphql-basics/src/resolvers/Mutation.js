cf0736f1220c5a8de613333b107f71adb0d00804
Ivan Malchanau <ivan.molchanoff@gmail.com>
2021-02-13 00:11:29 +0300

    update and delete post subscription

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -79,26 +79,42 @@ const Mutation = {
     db.posts.push(post);
 
     if (post.published) {
-      pubsub.publish('post', { post });
+      pubsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post,
+        },
+      });
     }
 
     return post;
   },
 
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex((post) => post.id === args.id);
     if (postIndex === -1) {
       throw new Error('Post not found');
     }
 
     db.comments = db.comments.filter((comment) => comment.post !== args.id);
-    const deletedPosts = db.posts.splice(postIndex, 1);
-    return deletedPosts[0];
+    const [post] = db.posts.splice(postIndex, 1);
+
+    if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: post,
+        },
+      });
+    }
+
+    return post;
   },
 
-  updatePost(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pubsub }, info) {
     const { id, data } = args;
     const post = db.posts.find((post) => post.id === id);
+    const originalPost = { ...post };
 
     if (!post) {
       throw new Error('Post not found');
@@ -107,13 +123,37 @@ const Mutation = {
     if (typeof data.title === 'string') {
       post.title = data.title;
     }
+
     if (typeof data.body === 'string') {
       post.body = data.body;
     }
+    
     if (typeof data.published === 'boolean') {
       post.published = data.published;
-    }
 
+      if (originalPost.published && !post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost,
+          },
+        });
+      } else if (!originalPost.published && post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post,
+          },
+        });
+      }
+    } else if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post,
+        },
+      });
+    }
     return post;
   },
 
